9c22ed5fee7e9199f45e07209a7280c830febd1b
G. Vanessa Fotso <vanessuniq@gmail.com>
2020-01-27 23:57:15 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,11 @@
 # Write a speak_to_grandma method.
-def speak_to_grandma 
-  
+def speak_to_grandma phrase
+  if phrase != phrase.upcase
+    grandma = 'HUH?! SPEAK UP, SONNY!'
+    grandma
+  elsif phrase 
+    
+    
 end
 
 # Whatever you say to grandma, she should respond with
